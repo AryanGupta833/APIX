50a169554e05bcbf458223fcfc66d5a7c717993e
Aryan_Gupta <byteknight4@gmail.com>
2026-04-04 23:35:24 +0530

    fix: proper otel exporter config

--- src/main/java/com/Aryan/APIX/config/OpenTelemetryConfig.java
+++ src/main/java/com/Aryan/APIX/config/OpenTelemetryConfig.java
@@ -1,7 +1,11 @@
 package com.Aryan.APIX.config;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.*;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +13,23 @@ import org.springframework.context.annotation.Configuration;
 public class OpenTelemetryConfig {
 
     @Bean
-    public Tracer tracer(){
-        return GlobalOpenTelemetry.getTracer("APIX-Tracer");
+    public OpenTelemetry openTelemetry() {
+
+        OtlpHttpSpanExporter exporter = OtlpHttpSpanExporter.builder()
+                .setEndpoint("https://all-in-one-production-a9d1.up.railway.app/v1/traces")
+                .build();
+
+        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+                .addSpanProcessor(SimpleSpanProcessor.create(exporter))
+                .build();
+
+        return OpenTelemetrySdk.builder()
+                .setTracerProvider(tracerProvider)
+                .build();
+    }
+
+    @Bean
+    public Tracer tracer(OpenTelemetry openTelemetry) {
+        return openTelemetry.getTracer("APIX-Tracer");
     }
 }
